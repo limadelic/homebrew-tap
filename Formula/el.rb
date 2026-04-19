682da2b50dbc8e14bb7e9842493f10ab0b197a37
msuarz <maykel.suarez@ukg.com>
2026-04-19 16:03:15 -0400

    simplify to arm64 only

--- Formula/el.rb
+++ Formula/el.rb
@@ -4,20 +4,11 @@ class El < Formula
   license "MIT"
   version "0.1.10"
 
-  if Hardware::CPU.arm?
-    url "https://github.com/limadelic/el/releases/download/v0.1.10/el_macos_arm64"
-    sha256 "a67caf88ec633323aa7f6eacda8e8f1fc7057dc82b2366273c88d48baedae96f"
-  else
-    url "https://github.com/limadelic/el/releases/download/v0.1.10/el_macos_x86_64"
-    sha256 "529597bf68bfedc5d7de01423c4fbd153fa863b06321e6c8b7ab42b84017a43e"
-  end
+  url "https://github.com/limadelic/el/releases/download/v0.1.10/el_macos_arm64"
+  sha256 "a67caf88ec633323aa7f6eacda8e8f1fc7057dc82b2366273c88d48baedae96f"
 
   def install
-    if Hardware::CPU.arm?
-      bin.install "el_macos_arm64" => "el"
-    else
-      bin.install "el_macos_x86_64" => "el"
-    end
+    bin.install "el_macos_arm64" => "el"
   end
 
   test do
