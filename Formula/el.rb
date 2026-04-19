19953ee52fb992812f5dbedf36120307e8bda3aa
Maykel Suarez <msuarz@limadelic.com>
2026-04-19 11:23:49 -0400

    Add real SHA256 hashes for v0.1.0 release

--- Formula/el.rb
+++ Formula/el.rb
@@ -6,11 +6,11 @@ class El < Formula
   on_macos do
     on_arm do
       url "https://github.com/limadelic/el/releases/download/v0.1.0/el_macos_arm64"
-      sha256 "TODO_ARM64_SHA256"
+      sha256 "5ef582e8847af735d74e8525187143ae1270ae26792b0254880c82499295a123"
     end
     on_intel do
       url "https://github.com/limadelic/el/releases/download/v0.1.0/el_macos_x86_64"
-      sha256 "TODO_X86_64_SHA256"
+      sha256 "605ed0679cbc4b5a77a665eef03f53764389d6f05e4de3c950e82b2b1ad03efa"
     end
   end
 
