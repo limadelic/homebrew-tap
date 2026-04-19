fae6e411f771718488dde4ae57a58bd8010d284f
msuarz <maykel.suarez@ukg.com>
2026-04-19 18:10:01 -0400

    fix formula install

--- Formula/el.rb
+++ Formula/el.rb
@@ -4,19 +4,11 @@ class El < Formula
   license "MIT"
   version "0.1.14"
 
-  on_macos do
-    on_arm do
-      url "https://github.com/limadelic/el/releases/download/v0.1.14/el_macos_arm64"
-      sha256 "e81b56901386fadceef5dd82756c01b8b6ffac82a933f5f4f2377b67a3cf5313"
-    end
-    on_intel do
-      url "https://github.com/limadelic/el/releases/download/v0.1.14/el_macos_x86_64"
-      sha256 "529597bf68bfedc5d7de01423c4fbd153fa863b06321e6c8b7ab42b84017a43e"
-    end
-  end
+  url "https://github.com/limadelic/el/releases/download/v0.1.14/el_macos_arm64"
+  sha256 "e81b56901386fadceef5dd82756c01b8b6ffac82a933f5f4f2377b67a3cf5313"
 
   def install
-    bin.install "el_macos_#{arch}" => "el"
+    bin.install "el_macos_arm64" => "el"
   end
 
   test do
