742fa8c7abb7dadb6b9781d15b7066d5b4cd899f
msuarz <maykel.suarez@ukg.com>
2026-04-21 23:53:34 -0400

    Update el to 0.1.41

--- Formula/el.rb
+++ Formula/el.rb
@@ -2,10 +2,10 @@ class El < Formula
   desc "CLI for managing headless Claude Code sessions"
   homepage "https://github.com/limadelic/el"
   license "MIT"
-  version "0.1.40"
+  version "0.1.41"
 
-  url "https://github.com/limadelic/el/releases/download/v0.1.40/el"
-  sha256 "ebc3d37c340b26953decda3fb1534e1881f84be1bf20e3147ab94738986f527a"
+  url "https://github.com/limadelic/el/releases/download/v0.1.41/el"
+  sha256 "0fa25e92309a1057ef78d1e95891358caf3137ade3981dcc21c65e57015a3874"
 
   def install
     bin.install "el"
